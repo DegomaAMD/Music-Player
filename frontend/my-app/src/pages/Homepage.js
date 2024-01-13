290b8bb99d8bf4dced02e7e8318d52359192a24b
degomaamd <degoma.andria@gmail.com>
2024-01-13 22:57:02 +0800

    updated sidebar content and styles

--- frontend/my-app/src/pages/Homepage.js
+++ frontend/my-app/src/pages/Homepage.js
@@ -1,40 +1,80 @@
 import React from 'react'
 import '../App.css';
+import vizmakerMP_logo from '../assets/img/vizmakerMP_logo-transparent.png'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCrown } from '@fortawesome/free-solid-svg-icons';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { faMicrophone } from '@fortawesome/free-solid-svg-icons';
+import { faCrown, faMagnifyingGlass, faMicrophone, faHouse, faBell, faGear, faArrowRightFromBracket, faRectangleList } from '@fortawesome/free-solid-svg-icons';
 
+function MyVerticallyCenteredModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Add Playlist
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Centered Modal</h4>
+          <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+            consectetur ac, vestibulum at eros.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
 
 function Homepage() {
     const linkBtn = ['For You', 'Popular', 'Trend', 'Pop', 'EDM', 'Rock', 'More'];
+
+    const [modalShow, setModalShow] = React.useState(false);
   return (
     <>
     <Container fluid>
         <Row>
                 <Col xs={3} className='side-bar'>
-                <h1>VizkerMP</h1>   
+                    <div className='brand-name-container'>
+                        <img src={vizmakerMP_logo} alt='VizmakerMP logo'/>
+                        <h1>VizkerMP</h1>
+                    </div>
+                   
                 <nav>
-                    <div>
+                    <div className='menu-container'>
                         <h2>Menu</h2>
                         <ul>
-                            <li>Home</li>
-                            <li>Notification</li>
-                            <li>Settings</li>
-                            <li>Logout</li>
+                            <li><FontAwesomeIcon icon={faHouse} className='menu-icon'/> Home</li>
+                            <li><FontAwesomeIcon icon={faBell} className='menu-icon'/> Notification</li>
+                            <li><FontAwesomeIcon icon={faGear} className='menu-icon'/> Settings</li>
+                            <li><FontAwesomeIcon icon={faArrowRightFromBracket} className='menu-icon'/> Logout</li>
                         </ul>
                     </div>
                     
-                    <div>
+                    <div className='playlist-container'>
                         <h2>Playlist</h2>
+                        <ul>
+                            <li onClick={() => setModalShow(true)}className='add-new-playlist'><FontAwesomeIcon icon={faRectangleList} className='menu-icon'/> Add New +</li>
+                        </ul>
 
                     </div>
                 </nav>
+                <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
 
             </Col>
             <Col xs={9} className='main-content'>
